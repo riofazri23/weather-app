68292a8f8e643f5c0e90e4b1feec93162a610cda
Rio Fazri Septian <rfazri7@gmail.com>
2023-11-23 10:58:13 +0700

    membuat hourly forecast dengan openapi forecast

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ const getCurrentWeatherData = async()=>{
     return response.json()
 }
 
+const getHourlyForecast = async({name: city})=>{
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
+    const data = await response.json();
+    return data.list.map(forecast=>{
+        const {main:{temp, temp_max, temp_min}, dt, dt_txt, weather: [{ description, icon }] } = forecast;
+        return{temp, temp_max, temp_min, dt, dt_txt, description, icon};
+    })
+}
+
 const formatTemperature = (temp) => `${temp?.toFixed(1)}°C`
+const createIconUrl = (icon) => `https://openweathermap.org/img/wn/${icon}@2x.png`
 
 loadCurrentForecast = ({name, main: {temp, temp_max, temp_min}, weather: [{description}] }) =>{
     const currentForecastElement = document.querySelector("#current-forecast");
@@ -21,8 +31,39 @@ loadCurrentForecast = ({name, main: {temp, temp_max, temp_min}, weather: [{descr
     //         <p class="min-max-temp">High Low</p>
 }
 
+const loadHourlyForecast = (hourlyForecast) => {
+    console.log(hourlyForecast);
+    let dataFor12Hours = hourlyForecast.slice(1, 13);
+    const hourlyContainer = document.querySelector(".hourly-container");
+    let innerHTMLString = ``;
+
+    for(let {temp, icon, dt_txt} of dataFor12Hours){
+        innerHTMLString +=`<article>
+        <h2 class="time">${dt_txt.split(" ")[1]}</h2>
+        <img class="icon" src="${createIconUrl(icon)}" />
+        <p class="hourly-temp">${formatTemperature(temp)}</p>
+    </article>`
+    }
+
+    hourlyContainer.innerHTML = innerHTMLString;
+}
+
+const loadFeelsLike = ({main: {feels_like}}) => {
+    let container = document.querySelector("#feels-like");
+        container.querySelector(".feels-like-temp").textContent = formatTemperature(feels_like);
+}
+const LoadHumidity = ({main: {humidity}}) => {
+    let container = document.querySelector("#humidity");
+        container.querySelector(".humidity-value").textContent = `${humidity} %`;
+}
+
 document.addEventListener("DOMContentLoaded", async()=>{
    const currentWeather = await getCurrentWeatherData();
-   loadCurrentForecast(currentWeather)
+   loadCurrentForecast(currentWeather);
+   const hourlyForecast =  await getHourlyForecast(currentWeather);
+    loadHourlyForecast(hourlyForecast);
+    loadFeelsLike(currentWeather);
+    LoadHumidity(currentWeather);
+
 
 })
