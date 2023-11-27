1e1533fcfe0d10ddf8e94d20488579ed5dbee368
Rio Fazri Septian <rfazri7@gmail.com>
2023-11-27 14:56:15 +0700

    membuat 5 day forecast

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const API_KEY ="66bfe77dabe3f6cee628caada8c0f389";
 
+const DAYS_OF_THE_WEEK = ["sun","mon","tue","wed","thu","fri","sat"];
+
 const getCurrentWeatherData = async()=>{
     const city="Jakarta";
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
@@ -48,22 +50,71 @@ const loadHourlyForecast = (hourlyForecast) => {
     hourlyContainer.innerHTML = innerHTMLString;
 }
 
+const calculateDayWiseForecast = (hourlyForecast)=>{
+    let dayWiseForecast = new Map();
+    for(let forecast of hourlyForecast){
+        const[date] = forecast.dt_txt.split(" ");
+        const dayOfWeek = DAYS_OF_THE_WEEK[new Date(date).getDay()]
+        console.log(dayOfWeek);
+        if(dayWiseForecast.has(dayOfWeek)){
+            let forecastForTheDay = dayWiseForecast.get(dayOfWeek);
+            forecastForTheDay.push(forecast);
+            dayWiseForecast.set(dayOfWeek, forecastForTheDay)
+
+        }else{
+            dayWiseForecast.set(dayOfWeek, [forecast]);
+        }
+    }
+    console.log(dayWiseForecast);
+    for(let [key, value] of dayWiseForecast){
+        let temp_min = Math.min(...Array.from(value,val=>val.temp_min))
+        let temp_max = Math.max(...Array.from(value,val=>val.temp_max))
+
+        dayWiseForecast.set(key, {temp_min, temp_max, icon: value.find(v=> v.icon).icon});
+    }
+    console.log(dayWiseForecast);
+    return dayWiseForecast;
+}
+
+const loadFiveDayForecast = (hourlyForecast) =>{
+    console.log(hourlyForecast)
+    const dayWiseForecast = calculateDayWiseForecast(hourlyForecast)
+    const container = document.querySelector(".five-day-forecast-container");
+    let dayWiseInfo = "";
+    Array.from(dayWiseForecast).map(([day, {temp_max, temp_min, icon}],index)=>{
+
+       if(index <5){
+           dayWiseInfo += `<article class="day-wise-forecast">
+            <h3 class="day">${index===0? "today": day}</h3>
+            <img class="icon" src="${createIconUrl(icon)}" alt="icon for the forecast">
+            <p class="min-texp">${formatTemperature(temp_min)}</p>
+            <p class="max-temp">${formatTemperature(temp_max)}</p>
+        </article>`;
+       } 
+
+    });
+
+    container.innerHTML = dayWiseInfo;
+
+}
+
 const loadFeelsLike = ({main: {feels_like}}) => {
     let container = document.querySelector("#feels-like");
-        container.querySelector(".feels-like-temp").textContent = formatTemperature(feels_like);
+    container.querySelector(".feels-like-temp").textContent = formatTemperature(feels_like);
 }
 const LoadHumidity = ({main: {humidity}}) => {
     let container = document.querySelector("#humidity");
-        container.querySelector(".humidity-value").textContent = `${humidity} %`;
+    container.querySelector(".humidity-value").textContent = `${humidity} %`;
 }
 
 document.addEventListener("DOMContentLoaded", async()=>{
-   const currentWeather = await getCurrentWeatherData();
-   loadCurrentForecast(currentWeather);
-   const hourlyForecast =  await getHourlyForecast(currentWeather);
+    const currentWeather = await getCurrentWeatherData();
+    loadCurrentForecast(currentWeather);
+    const hourlyForecast =  await getHourlyForecast(currentWeather);
     loadHourlyForecast(hourlyForecast);
+    loadFiveDayForecast(hourlyForecast);
     loadFeelsLike(currentWeather);
     LoadHumidity(currentWeather);
-
-
+    
+    
 })
